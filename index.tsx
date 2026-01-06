7f87a1dd7f0b9f9d92eb4ae59d2c4ebca840c4ac
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2026-01-06 06:46:31 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
+const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
+
+// FIX: Removed <React.StrictMode> to prevent R3F double-mount glitches
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
